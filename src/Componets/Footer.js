534e533ac46d2e5536e8680276fb73b513e5d53a
diana.iminza <diana.iminza@ronforddigital.com>
2022-10-03 12:20:25 +0300

    my first commit

---
+++ src/Componets/Footer.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "../Style/footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+     
+      <div className="footer_setions">
+        <div className="sections">
+          <h6>LET US HELP YOU</h6>
+          <p>Stay Safe</p>
+          <p>Help Center</p>
+          <p>Payment Methods</p>
+        </div>
+
+        <div className="sections">
+          <h6>ABOUT JUMIA </h6>
+          <p>About us</p>
+          <p>Jumia Careers</p>
+          <p>IPR Protection Policy</p>
+        </div>
+
+        <div className="sections">
+          <h6>MAKE MONEY WITH US</h6>
+          <p>Sell on Jumia</p>
+          <p>Become a Sales Consultant</p>
+          <p>Become a Logistics Service Partner</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
